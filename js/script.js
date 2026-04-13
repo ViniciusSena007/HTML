6833d3459e26948a02632c74410abd04879a5465
VINICIUS DE SENA SANTANNA <santanna.vinicius@aluno.senac.rio>
2026-04-13 20:55:35 -0300

    atualização de hj

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
 // Função para verificar idade
-function verificarIdade(){
+function verificarIdade() {
     let idade = parseInt(document.getElementById("idade").value);
-    if(idade < 18){
+    if (idade < 18) {
         alert("Para viajar sozinho. é nescessário ser maior de idade")
     }
+}
+
+function enviarFormulario(event) {
+
+    event.preventDefault();
+
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+    let email = document.getElementById("email").value;
+    let telefone = document.getElementById("telefone").value;
+    let destino = document.getElementById("destino").value;
+    let dtIda = document.getElementById("dataIda").value;
+    let dtVolta = document.getElementById("dataVolta").value;
+
+    let resultado = document.getElementById("resultado")
+
+    resultado.innerHTML = `
+    <div class="card p-3 mt-3">
+       <h4> Dados de Cadastro </h4>
+            <p><strong>Nome:</strong> ${nome}</p>
+            <p><strong>Idade:</strong> ${idade}</p>
+            <p><strong>E-mail:</strong> ${email}</p>
+            <p><strong>Telefone:</strong> ${telefone}</p>
+            <p><strong>Destino:</strong> ${destino}</p>
+            <p><strong>Data de ida:</strong> ${dtIda}</p>
+            <p><strong>Data de volta:</strong> ${dtVolta}</p>
+    `;
 }
